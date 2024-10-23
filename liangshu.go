f82b37846f2583c5a6b6abb0daf33cb5ac67a32f
liyuyan <beky200605@qq.com>
2024-10-23 17:09:13 +0800

    twosum

---
+++ liangshu.go
@@ -0,0 +1,31 @@
+package main
+import "fmt"
+func twosum(nums []int,target int)[]int{
+	var NUMS []int
+	for i:=0,i<len(nums)；i++{
+		for j:=i+1;j<len(nums);j++{
+			if(nums[i]+nums[j]==target){
+				NUMS={i,j}
+			}
+		}
+		}
+		return NUMS 
+	}
+func main(){
+	var target,length int
+	var nums,NUMS []int
+	fmt.Println("请输入数组长度：")
+	fmt.Scanf("%d",&length)
+	fmt.Println("请输入数组：")
+	for k:=0;k<length;k++{
+		fmt.Scanf("%d",nums[k])
+	}
+	fmt.Println("请输入两数和：")
+	fmt.Scanf("%d",&target)
+	NUMS=twosum(nums,target)
+	if(len(NUMS)!=0){
+		fmt.Println(NUMS[ : ]
+	}
+}
+
+
